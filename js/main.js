718c70922a32f94f8c3bf77ffdbf43b51ed6ff96
MicheleSantoro20 <michelesantoro2000@icloud.com>
2023-02-22 16:58:34 +0100

    Fixed js code: apex to numbers was removed

--- js/main.js
+++ js/main.js
@@ -8,18 +8,18 @@ document.getElementById("age").innerHTML = `La tua età è: ${eta} Anni`;
 
 let prezzoDistanzaPercorsa = km * prezzoKm;
 
-const prezzo_Over60 = prezzoDistanzaPercorsa / "100" * "60";
+const prezzo_Over60 = prezzoDistanzaPercorsa / 100 * 60;
 
-const prezzo_Under20 = prezzoDistanzaPercorsa / "100" * "80";
+const prezzo_Under20 = prezzoDistanzaPercorsa / 100 * 80;
 
 console.log("Prezzo per Km: " + prezzoDistanzaPercorsa);
 
-if (eta >= "60") {
+if (eta >= 60) {
     prezzo_Over60;
     console.log ("Prezzo per over 60: " + prezzo_Over60.toFixed(2));
     document.getElementById("ticket").innerHTML = `Il prezzo del tuo biglietto è:  ${prezzo_Over60.toFixed(2)} €`;
 
-} else if (eta < "20") {
+} else if (eta < 20) {
     prezzo_Under20;
     console.log ("Prezzo per under 20 " + prezzo_Under20.toFixed(2));
     document.getElementById("ticket").innerHTML = `Il prezzo del tuo biglietto è: ${prezzo_Under20.toFixed(2)} €`;
